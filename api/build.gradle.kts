37e6997592b40e44289ab30be44aaa94606639c3
ajay <ajay.swami@panopaticinfotech.com>
2026-03-28 10:57:01 +0530

    Clean up formatting in api/build.gradle.kts

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,28 +1,18 @@
 plugins {
-
     alias(libs.plugins.kotlinMultiplatform)
-
     alias(libs.plugins.androidKotlinMultiplatformLibrary)
-
     alias(libs.plugins.androidLint)
-
     alias(libs.plugins.kotlinSerialization)
-
     id("maven-publish")
 }
 
 kotlin {
 
     androidLibrary {
-
         namespace = "com.aj.api"
-
         compileSdk = 36
-
         minSdk = 24
-
         compilerOptions {
-
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
@@ -30,51 +20,33 @@ kotlin {
     jvm()
 
     iosX64()
-
     iosArm64()
-
     iosSimulatorArm64()
 
     sourceSets {
 
         commonMain.dependencies {
-
             implementation(libs.kotlin.stdlib)
-
             implementation(libs.kotlinx.coroutines.core)
-
             implementation(libs.ktor.client.core)
-
             implementation(libs.kotlinx.serialization.json)
-
             implementation(libs.ktor.client.content.negotiation)
-
             implementation(libs.ktor.serialization.kotlinx.json)
-
             implementation(libs.ktor.client.logging)
-
             implementation(libs.multiplatform.settings.core)
-
             implementation(libs.multiplatform.settings.serialization)
         }
 
-
         androidMain.dependencies {
-
             implementation(libs.ktor.client.okhttp)
         }
 
-
         iosMain.dependencies {
-
             implementation(libs.ktor.client.darwin)
         }
 
-
         jvmMain.dependencies {
-
             implementation(libs.ktor.client.cio)
         }
-
     }
 }
