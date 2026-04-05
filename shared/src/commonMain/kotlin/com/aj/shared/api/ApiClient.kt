3b36dc0897452756c3cbecb1a254da3064c5af5f
ajay <ajay.swami@panopaticinfotech.com>
2026-04-05 14:36:11 +0530

    Update provideSettings to accept a name parameter and enhance PickedFile with utility functions

--- shared/src/commonMain/kotlin/com/aj/shared/api/ApiClient.kt
+++ shared/src/commonMain/kotlin/com/aj/shared/api/ApiClient.kt
@@ -1,5 +1,6 @@
 package com.aj.shared.api
 
+import com.aj.shared.picker.PickedFile
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.forms.FormBuilder
