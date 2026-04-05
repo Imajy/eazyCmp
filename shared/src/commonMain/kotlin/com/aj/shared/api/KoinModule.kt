0af6280d355751f1e3365df744389190d8ff4ee3
ajay <ajay.swami@panopaticinfotech.com>
2026-04-05 14:47:54 +0530

    Refactor settings provision to use a configurable global name and rename `ajayModule` to `eazyModule`.
    Specific changes include:
    *   Updated `provideSettings` expect/actual declarations to remove the name parameter, favoring a global `SETTINGS_NAME`.
    *   Added `initSettingsName` and updated `initEazyCmp` to allow configuration of the settings name.
    *   Renamed `ajayModule` to `eazyModule` in `KoinModule.kt`.
    *   Introduced a default `SETTINGS_NAME` variable in `KoinModule.kt`.

--- shared/src/commonMain/kotlin/com/aj/shared/api/KoinModule.kt
+++ shared/src/commonMain/kotlin/com/aj/shared/api/KoinModule.kt
@@ -6,7 +6,7 @@ import org.koin.dsl.module
 
 val coreModule = module {
     single { json }
-    single { provideSettings(SETTINGS_NAME) }
+    single { provideSettings() }
     single { SharedViewModel(settings = get(), json = get()) }
 
     single { ApiClient() }
